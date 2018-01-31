638b8d35b09bae92d6f2e2f706d1c86aa8854bc8
Utkarsh Jaiswal <utkarsh@Utkarshs-MacBook-Air.local>
2018-01-31 12:16:59 +0530

    fix
    css

--- assets/js/script.js
+++ assets/js/script.js
@@ -172,18 +172,20 @@
 
 
   function increaseValue() {
-      var value = parseInt(document.getElementById('number').value, 10);
+      alert("inc");
+      var value = parseInt(document.getElementById('number').innerHTML, 10);
       value = isNaN(value) ? 0 : value;
       value++;
-      document.getElementById('number').value = value;
+      document.getElementById('number').innerHTML = value;
   }
 
   function decreaseValue() {
-      var value = parseInt(document.getElementById('number').value, 10);
+      alert("dec");
+      var value = parseInt(document.getElementById('number').innerHTML, 10);
       value = isNaN(value) ? 0 : value;
       value < 1 ? value = 1 : '';
       value--;
-      document.getElementById('number').value = value;
+      document.getElementById('number').innerHTML = value;
   }
 
   $("#productsImg").slick({
